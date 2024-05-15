3bdf5efeef9b300dac44f13afcb50303dc262d2a
Ivan Lartsov <lartsov@gmail.com>
2024-05-15 09:10:24 +0300

    upd: processSms

--- app/src/main/java/com/lis355/retand/Sms.kt
+++ app/src/main/java/com/lis355/retand/Sms.kt
@@ -19,24 +19,20 @@ class SmsBroadcastReceiver(val smsHandler: (Sms) -> Unit) : BroadcastReceiver() 
 
 class SmsProcessor(private val telegramUserMessagesSendable: TelegramUserMessagesSendable) {
     fun processSms(sms: Sms) {
-        // попробуем найти в сообщении все последовательности из 3х и более цифр, в телеграмм сообщении напишем их после сообщения
+        // попробуем найти в сообщении все последовательности из 3х и более цифр
+        // в телеграмм сообщении заменим их разметкой
         // чтобы на них можно было нажать и сразу скопировать (с помощью markdown)
 
-        val messageLines = arrayListOf(
-            sms.address,
+        val message = arrayListOf(
+            sms.address
+                .trim(),
             System.lineSeparator(),
             System.lineSeparator(),
             sms.text
+                .replace(Regex("\\d{3,}")) { "<code>${it.value}</code>" }
+                .trim()
         )
-
-        val numberSequences = Regex("\\d{3,}").findAll(sms.text).map { it.value }
-        for (numberSequence in numberSequences) {
-            messageLines.add(System.lineSeparator())
-            messageLines.add(System.lineSeparator())
-            messageLines.add("<code>$numberSequence</code>")
-        }
-
-        val message = messageLines.joinToString("")
+            .joinToString("")
 
         telegramUserMessagesSendable.sendMessageToUser(message)
     }
